2d704325c27fafe981ef4e7d3e795f5de5a85bec
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-12-29 15:32:15 +0600

    add user route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,5 +17,6 @@ app.use(cors({credentials: true}))
 // Api endpoints
 app.get('/', (req, res) => res.send("Api call n"))
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, ()=> console.log(`Server started on port: ${port}`))
